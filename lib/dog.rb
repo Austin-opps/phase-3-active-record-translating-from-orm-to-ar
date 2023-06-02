02cbd97b221d13dfb542ef1d061e732cec2eabf4
Austin <akomen04@gmail.com>
2023-06-02 13:13:18 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,2 @@
 class Dog < ActiveRecord::Base
-  #   attr_accessor :name, :breed
-
-  #   def self.create(attributes)
-  #     dog = self.new
-  #     attributes.each do |key, value|
-  #       dog.send("#{key}=", value)
-  #     end
-  #     dog.save
-  #     dog
-  #   end
-
-  #   def self.update(dog)
-  #     # Implementation of updating the record in the database goes here
-  #     # ...
-  #     puts "Dog record updated: #{dog.name}"
-  #   end
-
-  #   def find_or_create_by
-
-  #   end
 end
